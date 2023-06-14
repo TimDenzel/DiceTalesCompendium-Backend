0a08cecd5ddbbd3862b5a26eb8a6364af120c266
Denzel <timdenzel1410@googlemail.com>
2023-06-14 20:39:54 +0200

    Initial Commit

---
+++ src/main/java/de/dicetales/DiceTalesCompendiumBackend/DiceTalesCompendiumBackendApplication.java
@@ -0,0 +1,13 @@
+package de.dicetales.DiceTalesCompendiumBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceTalesCompendiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceTalesCompendiumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dicetales/DiceTalesCompendiumBackend/DiceTalesCompendiumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dicetales.DiceTalesCompendiumBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceTalesCompendiumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
